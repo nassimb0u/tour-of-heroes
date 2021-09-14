eaa0875cd516b03ec35702df29fdc36e3438ca01
nassimb0u <in_boulechfar@esi.dz>
2021-09-14 19:48:02 +0100

    Make things work with hero detail as a library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+//import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DetailComponent } from 'ngx-hero-detail';
+
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  { path: 'detail/:id', component: HeroDetailComponent}
+  { path: 'detail/:id', component: DetailComponent}
 ];
 
 @NgModule({
